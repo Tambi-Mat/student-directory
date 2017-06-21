e614e5f868aa0247563ba84c5c4ec877acdd6f33
Tambi-Mat <caicaratambi@gmail.com>
2017-06-21 14:56:57 +0100

    Script refactored and strings interpolated

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+# Assign the value 11 into the variable student_count. So there is no source of confusion later.
+student_count = 11
+
 # First we print the list of Students
 
 puts "The Students of Villians Academy"
@@ -14,7 +17,5 @@ puts "The Joker"
 puts "Jeffory Baratheon"
 puts "Norman Bates"
 
-# Finally we print the total number of students
-print "Overall, we have "
-print 11
-puts " Great Students"
+# Finally we print the total number of students. Use string interpolation with #{}
+puts "Overall, we have #{student_count} great students"
